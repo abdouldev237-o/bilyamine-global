3f18e5666ad38ff696178cd7ecc0201506c0f52b
abdouldev237-o <abdouldev237@gmail.com>
2026-09-01 22:58:37 +0100

    fix sitemap

--- portfolio/sitemaps.py
+++ portfolio/sitemaps.py
@@ -1,10 +1,11 @@
 from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
+    
     def items(self):
-        return ['home', 'about', 'services'] # add your url names from urls.py
+        return ['/', '/about/', '/services/', '/contact/', '/projects/', '/a-propos', '/realisations', '/temoignages']
+
     def location(self, item):
-        return reverse(item)
+        return item
