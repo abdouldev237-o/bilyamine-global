d51ee68fd1f66f675e8c0907148b335cd3c11595
abdouldev237-o <abdouldev237@gmail.com>
2026-09-01 22:54:34 +0100

    Add google verification meta

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.contrib.sitemaps.views import sitemap
+from portfolio.sitemaps import StaticViewSitemap # we create next
+from django.views.generic import TemplateView
+
+sitemaps = {'static': StaticViewSitemap}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls', namespace='portfolio')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
